aca869bc13559c155725c365582dd2a0af7b4047
hassantarif <hassantarif4@gmail.com>
2022-03-06 22:04:37 +0100

    com cat model

---
+++ app/models/cat.rb
@@ -0,0 +1,44 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :integer          not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Cat < ApplicationRecord
+    CATS_COLORS = [
+        "Black",
+        "Red",
+        "Green",
+        "Yellow",
+        "White",
+        "Silver",
+        "Black Smiko"
+    ]
+    CAT_SEX = [
+        "F",
+        "M"
+    ]
+    validates :birth_date, presence: true
+    validates :color, presence: true, inclusion: {
+        in: CATS_COLORS,
+        message: "you choose is not supported"
+    }
+    validates :name, presence: true
+    validates :sex, presence: true, inclusion: {
+        in: CAT_SEX,
+        message: "you shoose is not supported, if your cat gay pliz contact 911 :)"
+    }
+    validates :description, presence: true
+
+    def age
+        # Time.zone.now.year - birth_date.year, This trick is works to months
+        ((Time.zone.now - birth_date.to_time) / 1.year.seconds ).floor
+    end
+end
