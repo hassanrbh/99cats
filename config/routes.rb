9c2b2491ea31f5823f6b26bfd5fbb770d0ff97f5
hassantarif <hassantarif4@gmail.com>
2022-03-08 21:53:44 +0100

    cat rental controller is up and ready

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :cats, only: [:index, :show, :new, :create, :edit, :update]
+  resources :cat_rental_requests, only: [:new, :create]
 end
