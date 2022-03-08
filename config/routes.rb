fdad06b611951144367671bedbc0080a5d8a66f0
hassantarif <hassantarif4@gmail.com>
2022-03-08 23:45:07 +0100

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :cats, only: [:index, :show, :new, :create, :edit, :update]
-  resources :cat_rental_requests, only: [:new, :create]
+  resources :cat_rental_requests, only: [:new, :create] do
+    member do
+      post :approve
+      post :deny
+    end
+  end
 end
