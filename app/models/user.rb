0526b6ca566cf41de0bb6985b692044d18143b5e
hassantarif <hassantarif4@gmail.com>
2022-03-11 18:56:44 +0100

    Changes in auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,21 +13,11 @@ class User < ApplicationRecord
   attr_reader :password
   
   validates :session_token, presence: true
-  after_initialize :ensure_session_token
   validates :email, presence: true
   validates :password_digest, presence: { message: ' can\'t be blank' }
   validates :password, length: {minimum: 6, allow_nil: true}
-  
-  def self.generate_session_token
-    SecureRandom::urlsafe_base64(16)
-  end
+  after_initialize :ensure_session_token
 
-  def reset_session_token!
-    self.session_token = self.class.generate_session_token
-    self.save!
-    self.session_token
-  end
-  
   def password=(password) # wen the user input a password directly runs to this password function to make it even more secure
     @password = password # set an instance method for storing the password and not persisted in the db
     self.password_digest = BCrypt::Password.create(password)
@@ -37,15 +27,27 @@ class User < ApplicationRecord
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
+  # generate a session token every time we called this function
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  # reset the session token every time we called this function
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
   # find the user by credentials
-  def self.find_by_credentials(email,password)
-    user =  User.find_by(:email => email)
-    return nil if user.nil?
+  def find_by_credentials!(email,password)
+    user = User.find_by(:email => email) # find the user 
+    return nil if user.nil? # return nil if the user is not found
     user.is_password!(password) ? user : nil
   end
 
-  private 
-
+  private
+  
   def ensure_session_token
     self.session_token ||= self.class.generate_session_token
   end
