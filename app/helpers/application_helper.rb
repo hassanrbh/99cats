44ac72a2bb8bffe6c29d3f6458119c9f8517730f
hassantarif <hassantarif4@gmail.com>
2022-03-14 20:56:40 +0100

    changes in color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+    def signin_url
+        "<a href=\"#{signin_url}\">Sign In</a>".html_safe
+    end
+    
+    def signup_url
+        "<a href=\"#{signup_url}\">Sign Up</a>".html_safe
+    end
+
+    def cat_link(cat)
+        short_description = cat.description.slice(0,20)
+        short_description += " ..." if short_description.length > 10
+        html = "<a href=\"#{cat_url(cat)}\">"
+        html += html_escape(cat.name) + "(#{html_escape(short_description)})"
+        html += "</a>"
+        html.html_safe
+    end
 end
